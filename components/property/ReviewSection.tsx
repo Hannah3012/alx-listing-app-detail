0f33aabb7104b98362cda4ca0ecdcf6d91e62216
Hanna Tariku <hannatariku1996@gmail.com>
2025-06-18 16:36:38 +0300

    created a Review section

--- components/property/ReviewSection.tsx
+++ components/property/ReviewSection.tsx
@@ -5,7 +5,11 @@ const ReviewSection: React.FC<{ reviews: any[] }> = ({ reviews }) => {
       {reviews.map((review, index) => (
         <div key={index} className="border-b pb-4 mb-4">
           <div className="flex items-center">
-            <img src={review.avatar} alt={review.name} className="w-12 h-12 rounded-full mr-4" />
+            <img
+              src={review.avatar}
+              alt={review.name}
+              className="w-12 h-12 rounded-full mr-4"
+            />
             <div>
               <p className="font-bold">{review.name}</p>
               <p className="text-yellow-500">{review.rating} stars</p>
